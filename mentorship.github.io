75ead673e79dc08cb35b4a1a6027cf61607041ed
steven <steven.lawrence2@mountsinai.org>
2020-08-06 15:24:27 -0400

    Added some clickable find mentor/mentee options

--- mentorship.github.io
+++ mentorship.github.io
@@ -1,5 +1,5 @@
 ---
-title: "mentorship.io"
+title: "mentorship.github.io"
 author: "Steven Lawrence"
 date: "8/6/2020"
 output: html_document
---
+++ references.R
@@ -0,0 +1,6 @@
+#how to create link clicks for top page direction
+#https://stackoverflow.com/questions/29541037/can-i-add-a-go-to-top-button-to-rmarkdown-rendered-html-document/29541843
+
+#how to center things
+#https://stackoverflow.com/questions/24677642/centering-image-and-text-in-r-markdown-for-a-pdf-report
+
